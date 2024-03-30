4ca16e5f3b9696b3eda5a406e811dcf2bd74ed65
adiouane <94860452+adiouane@users.noreply.github.com>
2024-03-30 05:53:09 +0000

    remove auth with google becuase there is no time left

--- app/(app)/(public)/login/page.tsx
+++ app/(app)/(public)/login/page.tsx
@@ -1,9 +1,11 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { signupUsingPassword, authenticateUsingPassword, authenticateUsingGoogle } from "@/lib/supabase.auth.client";
+import {
+  signupUsingPassword,
+  authenticateUsingPassword,
+} from "@/lib/supabase.auth.client";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -13,42 +15,21 @@ export default function Login() {
 
   const router = useRouter();
 
-  const supabase = createClientComponentClient(); // This is the client-side version of the supabase client
-
-  const [form, setForm] = useState({
-    email: "",
-    password: "",
-  });
-
-  const onChange = (e: any) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
-
   const handleLogin = async () => {
     // use authenticateUsingPassword instead of signInWithPassword
     //TODO:
     const { error } = await authenticateUsingPassword({ email, password });
-		if (!error) {
-			router.push("/");
-			router.refresh();
-		}
-    else {
-      alert(error.message);
-    }
-  };
-
-  const SingUpWithGoogle = async () => {
-    const google = await authenticateUsingGoogle();
-    if (google) {
-      console.log(google);
+    if (!error) {
       router.push("/");
       router.refresh();
+    } else {
+      alert(error.message);
     }
   };
 
   const handleRegister = async () => {
     alert("register");
-    const {  error } : any = await signupUsingPassword({
+    const { error }: any = await signupUsingPassword({
       username: username,
       email,
       password,
@@ -57,10 +38,9 @@ export default function Login() {
       alert(error.message);
     } else {
       window.location.href = "/";
-      setIsSignedIn(true)
+      setIsSignedIn(true);
     }
   };
-  
 
   return (
     <>
@@ -68,33 +48,37 @@ export default function Login() {
         <div className="mx-auto max-w-2xl px-4">
           <div className="flex flex-col items-center justify-center min-h-screen py-12 space-y-4">
             <div className="space-y-2 text-center">
-              <h1 className="text-3xl font-bold">Welcome to the Platform</h1>
+              <h1 className="text-2xl font-bold uppercase">
+                Welcome to
+                <span className="text-red-500 text-4xl">
+                  {" "}
+                  B<span className="text-blue-500 text-3xl">2</span>
+                  <span className="text-red-500 text-2xl">B </span>
+                </span>
+                <span className="text-2xl font-bold uppercase">Dashboard</span>
+              </h1>
               <p className="text-gray-500 dark:text-gray-400">
                 Enter your credentials to access your account
               </p>
             </div>
             <div className="w-full space-y-4 sm:max-w-[400px]">
               <div className="space-y-2">
-                {
-                  isSignedIn && (
-                    <label
-                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                      htmlFor="username"
-                    >
-                      Username
-                      <input 
-                        className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm "
-                        id="username"
-                        placeholder="John Doe"
-                        onChange={(e) => {
-                          setUsername(e.target.value);
-                        }}
-                      />
-                    </label>
-
-                    
-                  )
-                }
+                {isSignedIn && (
+                  <label
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    htmlFor="username"
+                  >
+                    Username
+                    <input
+                      className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm "
+                      id="username"
+                      placeholder="example"
+                      onChange={(e) => {
+                        setUsername(e.target.value);
+                      }}
+                    />
+                  </label>
+                )}
                 <label
                   className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                   htmlFor="email"
@@ -126,45 +110,36 @@ export default function Login() {
                   }}
                 />
               </div>
-              {
-                !isSignedIn &&
+              {!isSignedIn && (
                 <button
-                onClick={handleLogin}
-                className="inline-flex items-center justify-center rounded-md text-sm font-medium  w-full bg-black text-white h-10"
+                  onClick={handleLogin}
+                  className="inline-flex items-center justify-center rounded-md text-sm font-medium  w-full bg-black text-white h-10"
                 >
-                Sign in
-              </button>
-              
-              }
-              {
-                !isSignedIn &&
+                  Sign in
+                </button>
+              )}
+              {!isSignedIn && (
                 <button
-                onClick={() => 
-                  setIsSignedIn(true)
-                }
-                className="inline-flex items-center justify-center rounded-md text-sm font-medium  w-full bg-white text-black h-10"
-                >
-                Sign up
-              </button>
-              }
-              {
-                isSignedIn && (
-                  <button
-                  onClick={handleRegister} // Call handleRegister on click
+                  onClick={() => setIsSignedIn(true)}
                   className="inline-flex items-center justify-center rounded-md text-sm font-medium  w-full bg-white text-black h-10"
                 >
                   Sign up
-                  </button>
-                )
-              }
+                </button>
+              )}
+              {isSignedIn && (
+                <button
+                  onClick={handleRegister} // Call handleRegister on click
+                  className="inline-flex items-center justify-center rounded-md text-sm font-medium  w-full bg-blue-500 text-white h-10"
+                >
+                  Sign up
+                </button>
+              )}
             </div>
-            <button
-              // sing up with google
-              onClick={SingUpWithGoogle}
-              className="inline-flex items-center justify-center  rounded-md text-sm font-medium  w-full bg-black text-white h-10"
-            >
-              Sign up with Google
-            </button>
+            <div className="w-full space-y-2 flex flex-col items-center justify-center">
+              <a className="text-sm underline" href="#">
+                Forgot your password?
+              </a>
+            </div>
           </div>
         </div>
         <div className="h-screen w-1/2 bg-red-400">
